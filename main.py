fbc407ededbe409cb51ab0afd392f4358bbbdc60
Rey Cabatingan <coolcabatingan@yahoo.com>
2017-10-05 18:04:09 -0500

    user signup

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
-from flask import Flask, request, redirect, render template
-import cgi
+from flask import Flask, request, render_template
+
+
 
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
 
-form = ""
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Please Sign Up</title>
-    <style>
-      div {
-          margin-bottom: 5px;
-      }
-      label {
-          display: block;
-          float: left;
-          width: 150px;
-          text-align: right;
-          font-weight: bold;
-          margin-right: 10px;
-      }
-      input.submit {
-          margin-left: 160px;
-      }
-    </style>
-</head>
-<body>
-  <h1> Please Sign Up</h1>
-  <form action="/form-processing-script" method="post">
-    <div>
-      <label for="username">Username</label> <input type="text" name="username">
-    </div>
-    <div>
-      <label for="password">Password</label> <input type="password" name="password">
-    </div>
-    <div>
-      <input type="submit" class="submit" value="Submit">
-    </div>
-  </form>
-</body>
-</html>
 
 
+@app.route("/", methods=["POST"])
+
+def sign_up():
+
+   #We got the info from the form submitted by the user
+
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+   #Storage for our error messages
+
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
+
+    if username == "" or " " in username or len(username) < 3 or len(username) > 20:
+       username_error = "Invalid username"
+
+    if password == "" or " " in password or len(password) < 3 or len(password) > 20:
+       password_error = "Invalid password"
+
+    if verify == "" or verify != password:
+        verify_error = "Invalid verification"
+
+    if email != "":
+       if "@" not in email or "." not in email or " " in email or len(email) < 3 or len(email) > 20:
+               email_error = "Invalid email"
+
+    if email_error == "" and username_error == "" and verify_error == "" and password_error == "":
+       return render_template("welcome.html", username = username)
+
+    else:
+       return render_template("index.html", username_error = username_error
+                                          , password_error = password_error
+                                          , verify_error = verify_error
+                                          , email_error = email_error
+                                          , username = username
+                                          , email = email)
+
+              
+ 
+@app.route("/")
+
+def index():
+
+   return render_template("index.html")
+
+app.run()
